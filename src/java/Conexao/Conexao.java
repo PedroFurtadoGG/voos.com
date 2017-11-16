2d5399961836daee1fc86a85eba10b19d2c5345b
dthlogus <matheus.junio159@gmail.com>
2017-11-16 21:33:14 -0200

    CRUD Login

--- src/java/Conexao/Conexao.java
+++ src/java/Conexao/Conexao.java
@@ -6,14 +6,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
  
 public class Conexao {
-    public Connection con;
+    public  Connection con;
     public PreparedStatement ps;
     public ResultSet rs;
     public String SQL;
  
-    public void OpenDatabase()throws Exception{
+    public  void  OpenDatabase()throws Exception{
         Class.forName("com.mysql.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/voos.com";
+        String url = "jdbc:mysql://localhost:3306/voos?zeroDateTimeBehavior=convertToNull";
         String user = "root";  //Seu nome de usuário.
         String password = "";  //Sua senha, nesse caso o Banco esta sem senha.
         con = DriverManager.getConnection(url, user, password);
