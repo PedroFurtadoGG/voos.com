9d9eee279d3ea1d592daa0c4e0813d457818090a
sedordiga <sedordiga@gmail.com>
2017-11-18 15:10:27 -0200

    Criacao do modelo de usuario(PEDRO)

---
+++ src/java/model/Usuario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Usuario {
+    
+  private int id_usuario;
+  private String nome;
+  private String email;
+  private String senha;
+  private String tipo;
+  private Date data_nascimento;
+  private String telefone;
+  private int cpf;
+   
+}
