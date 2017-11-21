13ca112821850e5b2dd6bcb1e1f848f94b389a47
sedordiga <sedordiga@gmail.com>
2017-11-21 01:55:31 -0200

    novo metodo de login

--- src/java/Controle/FormBase.java
+++ src/java/Controle/FormBase.java
@@ -6,6 +6,7 @@
 package Controle;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import modelo.Usuario;
 import persistencia.UsuarioDAO;
 
@@ -14,4 +15,27 @@ import persistencia.UsuarioDAO;
  * @author Lenovo
  */
 public class FormBase {
+    public String efetuarLogin(HttpServletRequest req){
+        try {
+            String uEmail = req.getParameter("email");
+            String uSenha = req.getParameter("senha");
+            
+            Usuario u = new Usuario();
+            u.setEmail(uEmail);
+            u.setSenha(uSenha);
+            
+            UsuarioDAO dao = new UsuarioDAO();
+            dao.efetuarLogin(uEmail, uSenha);
+            HttpSession session = req.getSession(true);
+            session.setAttribute("PjtLoginFiltro_logado", true);
+            session.setAttribute("PjtLoginFiltro_nome", u.getNome());
+            session.setAttribute("PjtLoginFiltro_cargo", u.getTipo());
+            req.getRequestDispatcher("index.jsp");  
+            return "Login efetuado com sucesso" + session;
+            
+        } catch (Exception ex) {
+            return "Exceção:" + ex.getMessage();
+        }
+       
+    }
 }
